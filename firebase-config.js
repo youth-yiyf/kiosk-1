01d37118707e1c776dd166672291543d905a3a7c
youth-yiyf <sujiyouth@naver.com>
2025-07-08 14:53:46 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+var firebaseConfig = {
+  apiKey: "AIzaSyDSq0gDWVbPRR90O0baA4Ilfuxqgdw93Ok",
+  authDomain: "kioskst-d67ef.firebaseapp.com",
+  databaseURL: "https://kioskst-d67ef.firebaseio.com",
+  projectId: "kioskst-d67ef",
+  storageBucket: "kioskst-d67ef.appspot.com",
+  messagingSenderId: "406076353975",
+  appId: "1:406076353975:web:095c9097b71a01e7f84c32"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
